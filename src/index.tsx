0de9ad8e118bc9e887190a8f4610510027862d42
Bryan Lara <bralavar@gmail.com>
2023-06-08 10:33:51 -0500

    Configure folders and install bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+
+require('bootstrap/dist/js/bootstrap')
+
+import './styles/app.scss'
+import Home from './pages/Home'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 )
 
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function App(): React.JSX.Element {
+  return <div className="app">hi</div>
+}
+
+export default App
