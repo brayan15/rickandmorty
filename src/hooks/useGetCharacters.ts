0c50d1091817bd8e90d9c4063c309873a31257a7
Bryan Lara <bralavar@gmail.com>
2023-06-08 14:18:40 -0500

    Add types for service and hook to get characters

---
+++ src/hooks/useGetCharacters.ts
@@ -0,0 +1,40 @@
+import { useCallback, useState } from 'react'
+
+import { CharacterT } from 'src/types'
+import apiRestClient from 'src/service/api'
+
+type UseGetCharactersPaginatedT = {
+  isLoading: boolean
+  characters: CharacterT[]
+  getCharacters: () => Promise<void>
+  setPage: React.Dispatch<React.SetStateAction<number>>
+}
+
+const useGetCharactersPaginated = (name = ''): UseGetCharactersPaginatedT => {
+  const [characters, setCharacters] = useState<CharacterT[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [page, setPage] = useState<number>(1)
+
+  const getCharacters = useCallback(async () => {
+    try {
+      setIsLoading(true)
+
+      const { data } = await apiRestClient().getCharacters({ page, name })
+
+      setCharacters(data.results)
+    } catch (_) {
+      setCharacters([])
+    } finally {
+      setIsLoading(false)
+    }
+  }, [page, name])
+
+  return {
+    setPage,
+    isLoading,
+    characters,
+    getCharacters,
+  }
+}
+
+export default useGetCharactersPaginated
---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+import { AxiosResponse } from 'axios'
+
+export type CharacterExtraT = {
+  name: string
+  url: string
+}
+
+export type CharacterT = {
+  id: number
+  url: string
+  name: string
+  type: string
+  image: string
+  gender: string
+  status: string
+  species: string
+  created: string
+  episode: string[]
+  origin: CharacterExtraT
+  location: CharacterExtraT
+}
+
+export type PaginatedResponseT<T> = {
+  info: {
+    count: number
+    pages: number
+    next: string | null
+    prev: string | null
+  }
+  results: T[]
+}
+
+export type ServiceResponseT<T> = Promise<AxiosResponse<T>>
+
+export type QueryParamT = {
+  [key: string]: string | number
+}
