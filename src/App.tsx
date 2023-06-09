a8ab6e6e79e879bf9fdf69e51f789b0c8e9330f4
Bryan Lara <bralavar@gmail.com>
2023-06-09 07:59:49 -0500

    Configure react route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from 'src/pages/Home'
 import Nav from 'src/components/Nav'
 import Footer from 'src/components/Footer'
+import CharacterDetail from 'src/pages/CharacterDetail'
 
 export default function App(): React.JSX.Element {
   return (
-    <div className="app">
-      <Nav />
-      <main className="main">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Nav />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/character/:id" element={<CharacterDetail />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/CharacterDetail/index.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react'
+
+const CharacterDetail: FunctionComponent = () => {
+  return <p>Detail</p>
+}
+
+export default CharacterDetail
